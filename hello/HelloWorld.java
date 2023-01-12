76e12ebaf3f51ac7c090dd44673591f946db5855
Travis <travis.gham@gmail.com>
2023-01-11 20:54:38 -0600

    [home_week1] add helloWorld, helloGoodbye

---
+++ hello/HelloWorld.java
@@ -0,0 +1,10 @@
+/* *****************************************************************************
+ *  Name:              Travis Hamilton
+ *  Last modified:     January 10, 2023
+ **************************************************************************** */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
